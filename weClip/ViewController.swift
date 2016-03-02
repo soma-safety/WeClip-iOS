8970ea294574d7a4f1c8b979efb0aa3e97754b69
SunKyu <leesunkeuy@naver.com>
2016-03-02 18:13:07 +0900

    Initial Commit

---
+++ weClip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weClip
+//
+//  Created by SunKyu Lee on 3/2/16.
+//  Copyright © 2016 SunKyu Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
